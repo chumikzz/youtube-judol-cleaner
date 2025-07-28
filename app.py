beee41eba769aa96df1d6a08f8ed490c1da1b684
Chumikzz <brilyawan.dwi@gmail.com>
2025-07-28 16:10:36 +0700

    🔧 Update: Listening to Railway port

--- app.py
+++ app.py
@@ -153,4 +153,5 @@ def run_cleaner():
     return f"✅ {total_deleted} komentar spam berhasil dihapus pada {waktu}.<br>Log disimpan ke: <b>{log_filename}</b>"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Railway akan kasih PORT lewat env
+    app.run(host="0.0.0.0", port=port)
